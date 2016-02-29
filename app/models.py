7ed5f5c5c018049b67eaf1c08043c08dea8574a9
David Martillier <sk3tchk@gmail.com>
2016-02-29 17:31:46 +0000

    initial commit

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 
 class User (db.Model):
     userid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique = True)
+    image = db.Column(db.String, unique = True)
     fname = db.Column(db.String(15), unique = True)
     lname = db.Column(db.String(50), unique = True)
     age = db.Column(db.String(3), unique = True)
